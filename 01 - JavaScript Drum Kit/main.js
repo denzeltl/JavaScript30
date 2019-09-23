419f72a4ba01311f874221740f78ac30c15b43d6
Denzel <denzel706@yahoo.com.ph>
2019-09-24 00:35:23 +0800

    finish day 1 project

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,7 +1,11 @@
 window.addEventListener("keydown", e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.classList.add("playing");
+    key.addEventListener("transitionend", () => {
+        key.classList.remove("playing");
+    });
 });
