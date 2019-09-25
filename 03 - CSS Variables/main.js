6b9fc7d9917b1a85ff993580fcda0d67194c4fa5
Denzel <denzel706@yahoo.com.ph>
2019-09-26 00:11:56 +0800

    updated REAMDE

--- 03 - CSS Variables/main.js
+++ 03 - CSS Variables/main.js
@@ -3,7 +3,6 @@ const controlInputs = document.querySelectorAll(".controls input");
 function applyData() {
     const suffix = this.dataset.sizing || "";
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    console.log(this.name);
 }
 
 controlInputs.forEach(input => input.addEventListener("change", applyData));
