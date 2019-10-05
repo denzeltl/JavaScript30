8b20d1f91c92557a26b05a72354ec00140e7a48a
Denzel <denzel706@yahoo.com.ph>
2019-10-05 12:35:20 +0800

    finish day 5 project

---
+++ 06 - Type Ahead/README.md
@@ -0,0 +1,12 @@
+# Day 6 - Ajax Type Ahead
+
+**Completed on:** _10/05/2019_
+
+---
+
+## Things Learned:
+
+-   AJAX (Asychronous JavaScript And XML) means to update something in the page without reloading the site
+-   JS `fetch(URL)` fetches data (like image, text, JSON, etc.) from a URL
+-   JS `...array` spread operator targets all items in an array
+-   JS `new RegExp()` function creates a pattern for the passed in argument
---
+++ 06 - Type Ahead/main.js
@@ -0,0 +1,41 @@
+const cities = [];
+
+fetch("https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json")
+    .then(response => response.json())
+    .then(json => {
+        cities.push(...json);
+    });
+
+const searchBar = document.querySelector(".search");
+const list = document.querySelector(".suggestions");
+searchBar.addEventListener("change", displayInputtedWord);
+searchBar.addEventListener("keyup", displayInputtedWord);
+
+function matchInputtedWord(inputtedWord, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(inputtedWord, "gi");
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function displayInputtedWord() {
+    const matchArray = matchInputtedWord(this.value, cities);
+    const html = matchArray
+        .map(place => {
+            const regex = new RegExp(this.value, "gi");
+            const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+            const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+            return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+        })
+        .join("");
+    list.innerHTML = html;
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
