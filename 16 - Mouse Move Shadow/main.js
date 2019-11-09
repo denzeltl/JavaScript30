49bdb99f5e5e1e716808ab6cb2d0799f0eec2b0a
Denzel <denzel706@yahoo.com.ph>
2019-11-09 23:52:43 +0800

    setup day 16 project

---
+++ 16 - Mouse Move Shadow/main.js
@@ -0,0 +1,8 @@
+const hero = document.querySelector(".hero");
+const text = document.querySelector("h1");
+
+hero.addEventListener("mousemove", shadow);
+
+function shadow(e) {
+    console.log(e);
+}
