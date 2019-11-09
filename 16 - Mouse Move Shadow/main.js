8515262b6b0a6d64eec327e26db5d0688a5f74fb
Denzel <denzel706@yahoo.com.ph>
2019-11-10 00:19:10 +0800

    finish day 16 project

--- 16 - Mouse Move Shadow/main.js
+++ 16 - Mouse Move Shadow/main.js
@@ -1,8 +1,22 @@
 const hero = document.querySelector(".hero");
 const text = document.querySelector("h1");
+const walk = 100;
 
 hero.addEventListener("mousemove", shadow);
 
 function shadow(e) {
-    console.log(e);
+    const width = this.offsetWidth;
+    const height = this.offsetHeight;
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    let walkX = Math.round((x / width) * walk - walk / 2);
+    let walkY = Math.round((y / height) * walk - walk / 2);
+
+    text.style.textShadow = `${walkX}px ${walkY}px 0 #bbb`;
 }
